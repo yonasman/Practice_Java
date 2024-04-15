1537b134aa0fdfec2337a65c607ce03b02f2233e
Yonas Negese <yonasnegese250@gmail.com>
2024-04-15 20:51:25 +0300

    First Java program

---
+++ Main.java
@@ -0,0 +1,17 @@
+//my first java program
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");  //prints Hello world
+	String name = "Yonas";
+	int a = 5;
+	float b = 2.4f;
+	char c = 'A';
+	boolean d = true;
+
+	System.out.println(name);
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(d);
+    }
+}
