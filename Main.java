2a943b1b2bd2e944209afcf4b985350c58c979fe
Yonas Negese <yonasnegese250@gmail.com>
2024-04-15 22:59:39 +0300

    Add some practice

--- Main.java
+++ Main.java
@@ -13,5 +13,18 @@ public class Main {
 	System.out.println(b);
 	System.out.println(c);
 	System.out.println(d);
+	//working with string
+	String text = "Yonas is becoming a great programmer!";
+	System.out.println("The length of text is :" + text.length());
+	//expl2
+	String firstName = "Yonas";
+	String lastName = "Negese";
+	String fullName = firstName.concat(lastName);
+	System.out.println(fullName);
+	//for each loop
+	String[] arr = {"abe","yon","bel"};
+	for (String i : arr) {
+		System.out.println(i);
+	}
     }
 }
